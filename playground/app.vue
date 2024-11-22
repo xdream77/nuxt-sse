795fa6eb2c8905d6f24b962d43e876b60a7e8e64
AndreKotthaus <andre.kotthaus@gmail.com>
2024-11-22 18:33:50 +0100

    Fu lint

--- playground/app.vue
+++ playground/app.vue
@@ -10,10 +10,10 @@ const msg = shallowRef([{}])
 if (import.meta.client) {
     const { $sse } = useNuxtApp()
     $sse.connect()
-    
+
     $sse.emitter.on('server:update', message => {
         if (message.event === 'hello') {
-            
+
             msg.value = [...msg.value, message.data]
         }
     })
